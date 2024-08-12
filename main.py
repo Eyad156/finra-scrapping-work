115102d1c0c63d8fd3614d1dc690453a584be947
Eyad156 <eyad1509@gmail.com>
2024-08-12 04:34:51 +0300

    000

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def fetch_broker_dealer_list():
         broker_dealers = []
         
         for item in broker_dealer_list:
-            broker_dealers.append(item.text.strip())
+            broker_dealers.append(item.text.strip(),'\n')
         
         # Save the extracted data to a CSV file
         with open('broker_dealer_list.csv', 'w', newline='') as csvfile:
